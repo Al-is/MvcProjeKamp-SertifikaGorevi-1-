a3522792d8dc66e605f0f7655fbea2ff4b9a24f4
Ali Can <cucu.alican@gmail.com>
2021-04-23 23:32:42 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        private GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repository.List();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length < 3 || p.CategoryDescription == "" || p.CategoryName.Length > 51)
+            {
+                //Hata mesajı
+            }
+            else
+            {
+                repository.Insert(p);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        private Context c = new Context();
+        private DbSet<Category> _object;
+
+        public List<Category> CategoriesList()
+        {
+            return _object.ToList();
+        }
+
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            c.SaveChanges();
+        }
+    }
+}
